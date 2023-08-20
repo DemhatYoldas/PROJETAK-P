23dc83dbfd0bad4ff433c72d6769ac900823af8a
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2023-08-20 18:43:05 +0300

    Personel Bilgileri

---
+++ PROJETAKİP/Models/Personel/PersonelBilgileri.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PROJETAKİP.Models.Personel
+{
+    public class PersonelBilgileri
+    {
+        [Key]
+        public int PersonelBilgiId { get; set; }
+
+        public string Eposta { get; set; }
+
+        public string Sifre { get; set; }
+
+        public string Yetki { get; set; }
+
+        public string AdSoyad { get; set; }
+
+        public string TCNO { get; set; }
+
+        public string Departman { get; set; }
+
+        public string Gorev { get; set; }
+
+        public string PozisyonAciklama { get; set; }
+
+        public string TelNo { get; set; }
+
+        public string Adres { get; set; }
+
+        public string MedeniHali { get; set; }
+
+        public string YakinBilgisi { get; set; }
+
+        public string YakinTC { get; set; }
+
+        public string YakinAdSoyad { get; set; }
+
+        public string YakinTel { get; set; }
+
+        public DateTime DogumTarihi { get; set; }
+
+        public DateTime? IseGirisTarihi { get; set; }
+
+    }
+}
