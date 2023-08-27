e3305a192555cc1d8354d1d0149b41e3af4aa0ee
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2023-08-28 01:59:37 +0300

    PersonelProje

---
+++ PROJETAKİP/Controllers/PersonelProjeController.cs
@@ -0,0 +1,25 @@
+﻿using PROJETAKİP.Models.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PROJETAKİP.Controllers
+{
+    public class PersonelProjeController : Controller
+    {
+        ProjeTakipDBContex db=new ProjeTakipDBContex();
+        // GET: PersonelProje
+        public ActionResult Index()
+        {
+            var proje = db.personelProjeleris.ToList();
+            return View(proje);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
