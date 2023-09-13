a9046d7109b15ee80bb2390ef69bc80f701e6bd6
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2023-09-14 02:29:42 +0300

    Personel Create

--- PROJETAKİP/Controllers/PersonelProjeController.cs
+++ PROJETAKİP/Controllers/PersonelProjeController.cs
@@ -1,4 +1,5 @@
 ﻿using PROJETAKİP.Models.DataContext;
+using PROJETAKİP.Models.ProjeTakip;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,7 +20,22 @@ namespace PROJETAKİP.Controllers
 
         public ActionResult Create()
         {
+            ViewBag.PersonelBilgiId = new SelectList(db.personelBilgileris, "PersonelBilgiId", "AdSoyad");
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Create(PersonelProjeleri projeObj, int[] PersonelBilgiId)
+        {
+            foreach (var x in PersonelBilgiId)
+            {
+                projeObj.PersonelBilgileris.Add(db.personelBilgileris.Find(x));
+            }
+            projeObj.OlusturulmaTarihi = DateTime.Now;
+            db.personelProjeleris.Add(projeObj);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
