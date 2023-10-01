062cd8107f7218dd218f11c2041da73cdd3f8215
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2023-10-01 07:42:55 +0300

    .

---
+++ PROJETAKİP/Controllers/GenelBakisController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PROJETAKİP.Controllers
+{
+    public class GenelBakisController : Controller
+    {
+        // GET: GenelBakis
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
