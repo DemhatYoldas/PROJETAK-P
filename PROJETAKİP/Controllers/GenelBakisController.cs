acca3e7e80073e048c99188b86e235e14687acd2
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2023-10-09 03:50:38 +0300

    viewbag

--- PROJETAKİP/Controllers/GenelBakisController.cs
+++ PROJETAKİP/Controllers/GenelBakisController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PROJETAKİP.Models.DataContext;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,26 @@ namespace PROJETAKİP.Controllers
 {
     public class GenelBakisController : Controller
     {
+        private ProjeTakipDBContex db = new ProjeTakipDBContex(); // Veri bağlantısı
         // GET: GenelBakis
         public ActionResult Index()
         {
+            int projesayisi=db.personelProjeleris.Count();
+            ViewBag.projesayisi=projesayisi;
+
+            int tamamlanmisproje = db.personelProjeleris.Where(p => p.TamamlanmaOrani == 100).Count();
+            ViewBag.tamamlanmisproje=tamamlanmisproje;
+
+            var yuksekoncelikli = db.personelProjeleris.Where(p => p.OncelikDurumu == "Yüksek Öncelikli").Count();
+            ViewBag.yuksekoncelikli=yuksekoncelikli;
+
+
+            var ortaoncelikli = db.personelProjeleris.Where(p => p.OncelikDurumu == "Orta Öncelikli").Count();
+            ViewBag.ortaoncelikli = ortaoncelikli;
+
+            var dusukoncelikli = db.personelProjeleris.Where(p => p.OncelikDurumu == "Düşük Öncelikli").Count();
+            ViewBag.dusukoncelikli = dusukoncelikli;
+
             return View();
         }
     }
