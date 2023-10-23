da009359b1fa9e7de5dbbf03c7547ace823374a0
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2023-10-23 14:53:00 +0300

    signalR ChatBox

---
+++ PROJETAKİP/SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PROJETAKİP.SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
