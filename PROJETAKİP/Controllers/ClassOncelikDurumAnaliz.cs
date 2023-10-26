4bd664b7fe09867b3683e9decf14b66db5b9d3ba
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2023-10-27 01:00:34 +0300

    Proje raporlari

---
+++ PROJETAKİP/Controllers/ClassOncelikDurumAnaliz.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PROJETAKİP.Controllers
+{
+    public class ClassOncelikDurumAnaliz
+    {
+        public string onceliktipi { get; set; }
+        public int oncelikadeti { get; set; }
+
+    }
+}
--- PROJETAKİP/Controllers/ProjeRaporlariController.cs
+++ PROJETAKİP/Controllers/ProjeRaporlariController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PROJETAKİP.Models.DataContext;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,74 @@ namespace PROJETAKİP.Controllers
 {
     public class ProjeRaporlariController : Controller
     {
-        // GET: ProjeRaporlari
-        public ActionResult Index()
+        private ProjeTakipDBContex db = new ProjeTakipDBContex();  //VERİ TABANI BAĞLANTISI
+
+        public ActionResult TamamalanmisOncelikGruplari()
         {
             return View();
         }
+        public ActionResult VisualizeSTamamlanmisDurumGruplari()
+        {
+            return Json(OncelikGrupTİpi(), JsonRequestBehavior.AllowGet);
+        }
+        public List<ClassOncelikDurumAnaliz> OncelikGrupTİpi()
+        {
+            ;
+            List<ClassOncelikDurumAnaliz> snf = new List<ClassOncelikDurumAnaliz>();
+            using (var c = new ProjeTakipDBContex())
+
+                snf = c.personelProjeleris.Where(x => x.TamamlanmaDurumu == true).GroupBy(p => p.OncelikDurumu).Select(x => new ClassOncelikDurumAnaliz
+                {
+
+                    onceliktipi = x.Key,
+                    oncelikadeti = x.Count(),
+                }).ToList();
+
+            return snf;
+        }
+
+
+
+
+        public ActionResult TamamalanmamisOncelikGruplari()
+        {
+            return View();
+        }
+        public ActionResult VisualizeTamamlanmayanDurumruplari()
+        {
+            return Json(OncelikTamamlanmisGrupTİpi(), JsonRequestBehavior.AllowGet);
+        }
+        public List<ClassOncelikDurumAnaliz> OncelikTamamlanmisGrupTİpi()
+        {
+            ;
+            List<ClassOncelikDurumAnaliz> snf = new List<ClassOncelikDurumAnaliz>();
+            using (var c = new ProjeTakipDBContex())
+
+                snf = c.personelProjeleris.Where(x => x.TamamlanmaDurumu == false).GroupBy(p => p.OncelikDurumu).Select(x => new ClassOncelikDurumAnaliz
+                {
+
+                    onceliktipi = x.Key,
+                    oncelikadeti = x.Count(),
+                }).ToList();
+
+            return snf;
+        }
+
+
+        public ActionResult GenelProjeRaporlari()
+        {
+            return View();
+        }
+
+
+        public ActionResult CanliDestek()
+        {
+            var destek = db.personelBilgileris.Where(x => x.Departman == "Yönetim");
+            return View(destek.ToList());
+        }
+
+
     }
+
+
 }
